0653c188dbf7258522a1ec16ae873c1905a64ede
Darrell Pawson <dpawson905@gmail.com>
2021-10-23 14:39:58 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const terser = require("gulp-terser");
+const { src, dest, watch, series } = require("gulp");
+const autoprefixer = require('autoprefixer');
+const cssnano = require("cssnano");
+const browersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+  var plugins = [
+    autoprefixer(),
+    cssnano()
+  ]
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(dest("dist/css", { sourcemaps: "." }));
+}
+
+// Javascript Task
+function jsTask() {
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist/js", { sourcemaps: "." }));
+}
+
+// BrowserSync Task
+function browersyncServe(cb) {
+  browersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browersyncReload(cb) {
+  browersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browersyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browersyncReload)
+  );
+}
+
+// Defalt Gulp task
+exports.default = series(scssTask, jsTask, browersyncServe, watchTask);
